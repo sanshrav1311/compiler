02a71b93f312a9579940836e79af23cb57f2a143
Sanshrav Arora <sanshrav1311@gmail.com>
2024-02-28 21:50:30 +0530

    implemented linkedlist funcitons

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@ int hash(NONTERMINAL nt){
     return nt % GRAMMER_TABLE_SIZE;
 }
 
-typedef enum NONTERMINAL {program, main_function, other_functions, function, input_par, output_par, parameter_list, dataType, primitiveDatatype, constructedDatatype, remaining_list, stmts, typeDefinitions, typeDefinition, fieldDefinitions, fieldDefinition, moreFields, declarations, delaration, globar_or_not, otherStmts, stmt, assignmentStmt, singleOrRecId, B,funCallStmt, outputParameters, inputParameters, iterativeStmt, conditionalStmt, ioStmt, aritmeticExpression, addORsub, mulORdiv, brackets, MULDIV, ADDSUB, booleanExpression, var, logicalOp, relationalOp, returnStmt, optionalReturn, idList, more_ids, definetypestmt, A} NONTERMINAL;
+typedef enum NONTERMINAL {program, mainFunction, otherFunctions, function, input_par, output_par, parameter_list, dataType, primitiveDatatype, constructedDatatype, remaining_list, stmts, typeDefinitions, typeDefinition, fieldDefinitions, fieldDefinition, moreFields, declarations, delaration, globar_or_not, otherStmts, stmt, assignmentStmt, singleOrRecId, B,funCallStmt, outputParameters, inputParameters, iterativeStmt, conditionalStmt, ioStmt, aritmeticExpression, addORsub, mulORdiv, brackets, MULDIV, ADDSUB, booleanExpression, var, logicalOp, relationalOp, returnStmt, optionalReturn, idList, more_ids, definetypestmt, A} NONTERMINAL;
 typedef enum TOKENS {TK_ERROR, TK_ASSIGNOP, TK_COMMENT, TK_FIELDID, TK_ID, TK_NUM, TK_RNUM, TK_FUNID, TK_RUID, TK_WITH, TK_PARAMETERS, TK_END, TK_WHILE, TK_UNION, TK_ENDUNION, TK_DEFINETYPE, TK_AS, TK_TYPE, TK_MAIN, TK_GLOBAL, TK_PARAMETER, TK_LIST, TK_SQL, TK_SQR, TK_INPUT, TK_OUTPUT, TK_INT, TK_REAL, TK_COMMA, TK_SEM, TK_COLON, TK_DOT, TK_ENDWHILE, TK_OP, TK_CL, TK_IF, TK_THEN, TK_ENDIF, TK_READ, TK_WRITE, TK_RETURN, TK_PLUS, TK_MINUS, TK_MUL, TK_DIV, TK_CALL, TK_RECORD, TK_ENDRECORD, TK_ELSE, TK_AND, TK_OR, TK_NOT, TK_LT, TK_LE, TK_EQ, TK_GT, TK_GE, TK_NE} TOKENS;
 
 typedef struct GrammerElement{
@@ -35,8 +35,6 @@ typedef struct RHS {
     GrammerElementHead *grammerHead;
     RHS* next;
 } RHS;
-
-RHSHead* LHSmap[GRAMMER_TABLE_SIZE] = { NULL };
 
 RHSHead* createRHSHead() {
     RHSHead* newNode = (RHSHead*)malloc(sizeof(RHSHead));
@@ -92,9 +90,14 @@ void insertGrammerElement(GrammerElementHead* head, GrammerElement* ge) {
     head->count++;
 }
 
-//assign Grammerelement head to RHS 
+RHSHead* LHSmap[GRAMMER_TABLE_SIZE] = { NULL };
+void insertInHashTable(NONTERMINAL nt, RHSHead* rhsHead){
+    int index = hash(nt);
+    LHSmap[index] = rhsHead;
+}
 
 int main(int argc, char const *argv[]){
+    
 
     return 0;
 }
